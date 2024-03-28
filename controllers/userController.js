860092035b1e57efb23d649bd67c270c70c9502e
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-03-28 10:01:58 +0545

    ch

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,78 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+module.exports.userLogin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const existUser = await User.findOne({ email });
+    if (existUser) {
+      const pass = bcrypt.compareSync(password, existUser.password);
+      if (!pass) return res.status(401).json({ status: 'error', message: 'invalid credential' })
+
+      const token = jwt.sign({
+        id: existUser._id,
+        isAdmin: existUser.isAdmin
+      }, 'jwtsecret');
+
+      return res.status(200).json({
+        status: 'successfully login',
+        data: {
+          token,
+          id: existUser._id,
+          isAdmin: existUser.isAdmin,
+          email: existUser.email,
+          fullname: existUser.fullname,
+          shippingAddress: existUser.shippingAddress
+        }
+      });
 
 
+    } else {
+      return res.status(401).json({
+        status: 'error',
+        message: `user doesn't exist`
+      });
+    }
 
+  } catch (err) {
+    return res.status(400).json({
+      status: 'error',
+      message: `${err}`
+    });
 
-module.exports.useLogin = (req, res) => {
+  }
 
 }
 
-module.exports.useRegister = (req, res) => {
+module.exports.useRegister = async (req, res) => {
+  const { email, fullname, password } = req.body;
+  try {
+    const existUser = await User.findOne({ email });
+    if (existUser) {
+      return res.status(401).json({
+        status: 'error',
+        message: `user already exist`
+      });
+    } else {
+      const hashPassword = bcrypt.hashSync(password, 10);
 
+      await User.create({
+        email,
+        password: hashPassword,
+        fullname
+      });
+      return res.status(201).json({
+        status: 'success',
+        message: 'user successfully registered'
+      });
+    }
+
+  } catch (err) {
+    return res.status(400).json({
+      status: 'error',
+      message: `${err}`
+    });
+
+  }
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const { userLogin, useRegister } = require('../controllers/userController');
+const validator = require('express-joi-validation').createValidator({});
+const Joi = require('joi');
 const router = express.Router();
 
 
 
-router.route('/users/login').post();
-router.route('/users/register').post();
+const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(5).max(20).required()
+});
+
+const registerSchema = Joi.object({
+  email: Joi.string().email().required('mail is req'),
+  password: Joi.string().min(5).max(20).required(),
+  fullname: Joi.string().required(),
+});
+
+
+
+
+
+router.route('/users/login').post(validator.body(loginSchema), userLogin);
+router.route('/users/register').post(validator.body(registerSchema), useRegister);
 router.route('/users').get();
 router.route('/users/:id').get().patch();
 
